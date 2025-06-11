b002152911a468ae883f7937380194395290e16e
Inyeccion <1607569403@qq.com>
2025-06-11 22:17:05 +0800

    M1.1 : definitive edition

--- my_visualize.py
+++ my_visualize.py
@@ -19,8 +19,7 @@ class InferenceConfig(Config):
     NAME = "coco"
     GPU_COUNT = 1
     IMAGES_PER_GPU = 1
-    # 其它参数根据你的训练配置调整
-    NUM_CLASSES = 1 + 6  # 背景+你训练的类别数
+    NUM_CLASSES = 1 + 6  # 背景+训练的类别数
 
 config = InferenceConfig()
 
@@ -28,11 +27,11 @@ config = InferenceConfig()
 model = modellib.MaskRCNN(mode="inference", config=config, model_dir="logs")
 
 # 加载权重
-model_path = "logs/coco20250610T1616/mask_rcnn_coco_0001.h5"  # 换成你的权重路径
+model_path = "logs/coco20250610T1616/mask_rcnn_coco_0001.h5"  # 预训练权重路径
 model.load_weights(model_path, by_name=True)
 
 # 加载图片
-image = skimage.io.imread("test2.jpg")  # 换成你的图片路径
+image = skimage.io.imread("test2.jpg")  # 测试用图片路径
 
 # 推理
 results = model.detect([image], verbose=1)
@@ -44,7 +43,7 @@ print("每个目标的分割掩码 shape：", r['masks'].shape)
 print("每个目标的置信度：", r['scores'])
 
 # 类别名列表，需训练时的类别顺序一致
-class_names = ['BG', 'person', 'car', 'bus', 'truck', 'bicycle', 'motorcycle']  # 示例
+class_names = ['BG', 'person', 'car', 'bus', 'truck', 'bicycle', 'motorcycle']  
 
 visualize.display_instances(
     image, r['rois'], r['masks'], r['class_ids'],
